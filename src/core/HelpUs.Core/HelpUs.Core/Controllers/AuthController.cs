36fd5880bd650eb3901cfe55aa6d856414560e4d
Felipe Fernandes de Aquino <fernandes35felipe@gmail.com>
2021-05-28 21:14:03 -0300

    agora sim

---
+++ src/core/HelpUs.Core/HelpUs.Core/Controllers/AuthController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NetDevPack.Identity.Jwt;
+using NetDevPack.Identity.Jwt.Model;
+using NetDevPack.Identity.Model;
+
+namespace HelpUs.Core.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppJwtSettings _appJwtSettings;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+              UserManager<IdentityUser> userManager,
+              IOptions<AppJwtSettings> appJwtSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appJwtSettings = appJwtSettings.Value;
+        }
+
+        [HttpPost("cadastrar")]
+        public async Task<ActionResult> Register([FromBody] RegisterUser registerUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(GetFullJwt(registerUser.Email));
+            }
+
+            return BadRequest(result.Errors);
+
+        }
+
+        [HttpPost("logar")]
+        public async Task<ActionResult> Login([FromBody] LoginUser loginUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok(GetFullJwt(loginUser.Email));
+            }
+
+            if (result.IsLockedOut)
+            {
+                return BadRequest("Usuário bloqueado, tente novamente mais tarde");
+            }
+
+            return BadRequest("E-mail ou Senha incorretos");
+
+        }
+
+        //Um token
+        private string GetFullJwt(string email)
+        {
+            return new JwtBuilder()
+                .WithUserManager(_userManager)
+                .WithJwtSettings(_appJwtSettings)
+                .WithEmail(email)
+                .BuildToken();
+
+            /*
+             * Mais opções
+             return new JwtBuilder()
+                .WithUserManager(_userManager)
+                .WithJwtSettings(_appJwtSettings)
+                .WithEmail(email)
+                .WithJwtClaims()
+                .WithUserClaims()
+                .WithUserRoles()
+                .BuildToken();
+             */
+        }
+
+        //Objeto com tokens
+        private UserResponse GetUserResponse(string email)
+        {
+            return new JwtBuilder()
+                .WithUserManager(_userManager)
+                .WithJwtSettings(_appJwtSettings)
+                .WithEmail(email)
+                .WithJwtClaims()
+                .WithUserClaims()
+                .WithUserRoles()
+                .BuildUserResponse() as UserResponse;
+        }
+
+    }
+}

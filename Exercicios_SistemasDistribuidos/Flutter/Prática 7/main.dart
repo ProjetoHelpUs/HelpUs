5ecd16eeeb57ce783faa7d4a7f9a4a400503a538
DinelliFernanda <45576842+DinelliFernanda@users.noreply.github.com>
2021-06-15 10:45:04 -0300

    Add files via upload

---
+++ Exercicios_SistemasDistribuidos/Flutter/Prática 7/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Container(
+        child: Image(
+          image: NetworkImage(
+            'https://i.picsum.photos/id/37/250/250.jpg?hmac=ZMIo0U9qQ6H3-jXK1khrSHzz-JeUuFQw5Hu0VO8Sdjg',
+          ),
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.yellow,
+        ),
+        padding: EdgeInsets.all(100),
+        margin: const EdgeInsets.all(15),
+      ),
+    ),
+  );
+}
